a79f66c32ce0d7980418041b6adbd95aa3554a91
Jakub Kołuda <jko01@outlook.com>
2023-11-18 13:59:21 +0100

    Add editing category functionality.

--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -13,41 +13,75 @@ module.exports = createCoreController(
   ({ strapi }) => ({
     async create(ctx) {
       const user = ctx.state.user;
-      const { category, color } = ctx.request.body;
+      const { category, color, id } = ctx.request.body;
       try {
+        console.log(id);
         const categoriesEntity = await strapi.services[
           "api::category.category"
         ].find({
           filters: { user: user },
         });
         console.log(categoriesEntity);
+
         let categoryExists = false;
         categoriesEntity.results.forEach((item) => {
           if (item.name.toLowerCase().trim() === category.toLowerCase()) {
             categoryExists = true;
           }
         });
+
         if (!categoryExists) {
-          const entity = await strapi.entityService.create(
-            "api::category.category",
-            {
-              data: {
-                user,
-                name: category.trim(),
-                color,
-              },
-            }
-          );
-          const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-          return this.transformResponse(sanitizedEntity);
+          if (id) {
+            const entity = await strapi.entityService.update(
+              "api::category.category",
+              id,
+              {
+                data: {
+                  name: category.trim(),
+                  color,
+                },
+              }
+            );
+            const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+            return this.transformResponse(sanitizedEntity);
+          } else {
+            const entity = await strapi.entityService.create(
+              "api::category.category",
+              {
+                data: {
+                  user,
+                  name: category.trim(),
+                  color,
+                },
+              }
+            );
+            const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+            return this.transformResponse(sanitizedEntity);
+          }
         } else {
-          throw new ValidationError("Category exists");
+          if (id) {
+            const entity = await strapi.entityService.update(
+              "api::category.category",
+              id,
+              {
+                data: {
+                  name: category.trim(),
+                  color,
+                },
+              }
+            );
+            const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+            return this.transformResponse(sanitizedEntity);
+          } else {
+            throw new ValidationError("Category exists");
+          }
         }
       } catch (err) {
         console.log(err.name);
         return this.transformResponse({ error: err.name });
       }
     },
+
     async find(ctx) {
       try {
         const user = ctx.state.user.id;
